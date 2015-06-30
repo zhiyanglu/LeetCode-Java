ccfb6fca1c2bea7cdcb2cd9ccc90bcefec7a3dc1
zhiyanglu <lu_zhiyang@hotmail.com>
2015-06-29 23:32:58 -0700

    Word Search||
    Accepted solution. Add find prefix to decrease useless backtracking.

--- WordSearchII.java
+++ WordSearchII.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Given a 2D board and a list of words from the dictionary, 
@@ -23,25 +25,25 @@ public class WordSearchII {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		char[][] board = {
-				  {'o','a','a','n','a','n'},
-				  {'e','t','a','e','a','n'},
-				  {'i','h','k','r','a','n'},
-				  {'i','f','l','v','a','n'},
-				  {'i','f','l','v','a','n'},
+				  {'o','a','a','n'},
+				  {'e','t','a','e'},
+				  {'i','h','k','r'},
+				  {'i','f','l','v'},
+				  {'i','f','l','v'},
 		};
 		String[] words = {"athfiieo"};
 		System.out.println(new WordSearchII().findWords(board, words));
 	}
 
 	
-	
-	
-	
-	
-	
-	
 	/**
-	 * Use backtracking and Trie Tree --- Slow soution
+	 * Use backtracking and Trie Tree
+	 * Put all words in Trie Tree
+	 * Do backtracking on board
+	 * if the trie tree contains current prefix 
+	 * 		search each possible word in tree and keep backtracking
+	 * else 
+	 * 		return to previous recursion
 	 */
 	private TrieNode root = new TrieNode();
 	class TrieNode {
@@ -65,45 +67,64 @@ public class WordSearchII {
 				return false;
 			cur = cur.children[c - 'a'];
 		}
-		return !cur.item.equals("");
+		return cur.item.equals(word);
 	}
+	public boolean startWith(String word){
+		TrieNode cur = root;
+		for(char c : word.toCharArray()){
+			if(cur.children[c - 'a'] == null)
+				return false;
+			cur = cur.children[c - 'a'];
+		}
+		return true;
+	}
+	
+	List<String> result;
+	Set<String> set;
 	public List<String> findWords(char[][] board, String[] words) {
-		List<String> result = new ArrayList<String>();
+		result = new ArrayList<String>();
+		set = new HashSet<String>();
 		if (board.length == 0)
 			return result;
 		boolean[][] used = new boolean[board.length][board[0].length];
 
 		StringBuilder sb = new StringBuilder();
 		// add all possible words to the TrieTree
+		for (int i = 0; i < words.length; i++) {
+			addWord(words[i]);
+		}
+		
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[0].length; j++) {
-				System.out.println(i + " " + j);
-
-				build(board, used, i, j, sb);
-				
+				search(board, used, i, j, sb);				
 			}
 		}
-		for (int i = 0; i < words.length; i++) {
-			if (searchWord(words[i])) {
-				result.add(words[i]);
-			}
+		for(String s : set){
+			result.add(s);
 		}
 		return result;
 	}
-	private void build(char[][] board, boolean[][] used, int x, int y,
+	private void search(char[][] board, boolean[][] used, int x, int y,
 			StringBuilder sb) {
-		addWord(sb.toString());
+
 		if (x < 0 || x >= board.length || y < 0 || y >= board[0].length)
 			return;
 		if (used[x][y])
 			return;
+		sb.append(board[x][y]);
+		if(!startWith(sb.toString())){
+			sb.deleteCharAt(sb.length() - 1);
+			return;
+		}
 
 		used[x][y] = true;
-		sb.append(board[x][y]);
-		build(board, used, x + 1, y, sb);
-		build(board, used, x - 1, y, sb);
-		build(board, used, x, y + 1, sb);
-		build(board, used, x, y - 1, sb);
+		if(searchWord(sb.toString())){
+			set.add(sb.toString());
+		}
+		search(board, used, x + 1, y, sb);
+		search(board, used, x - 1, y, sb);
+		search(board, used, x, y + 1, sb);
+		search(board, used, x, y - 1, sb);
 		used[x][y] = false;
 		sb.deleteCharAt(sb.length() - 1);
 		return;
