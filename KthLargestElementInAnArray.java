d649178eb1c2637d72db79997e188477fc2f3f13
zhiyanglu <lu_zhiyang@hotmail.com>
2015-07-01 03:38:37 -0700

    Kath Largest Element In an Array
    Add O(n) best O(n^2) worst solution.

--- KthLargestElementInAnArray.java
+++ KthLargestElementInAnArray.java
@@ -13,18 +13,62 @@ public class KthLargestElementInAnArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] a = {3, 1, 2, 4};
+		int[] a = {7, 5, 6, 1, 3, 2, 4};
 		System.out.println(new KthLargestElementInAnArray().findKthLargest(a, 2));
 		
 	}
 	
+	/**
+	 * O(N) best case / O(N^2) worst case running time + O(1) memory solution
+	 * Based on quick sort partition
+	 */
+	public int findKthLargest(int[] nums, int k){
+		k = nums.length - k;	//change k to the position of target in a sorted array
+		int lo = 0;
+		int hi = nums.length - 1;
+		while(lo < hi){
+			//get the partition point position
+			final int j = partition(nums, lo, hi);
+			if(j < k){			//j is less than k, target is in right of j
+				lo = j + 1;
+			}else if(j > k){	//j is larger than k, target is in left of j
+				hi = j - 1;
+			}else{				//find target
+				break;
+			}
+		}
+		return nums[k];
+	}
+	/**
+	 * put the number x at a[0] to its sorted position by partition the array
+	 * and return the position.
+	 * all elements on left of x is less than it
+	 * all elements on right of x is larger than it
+	 */
+	private int partition(int[] a, int lo, int hi){
+		int i = lo + 1;
+		int j = hi + 1;
+		while(true){
+			//Start from left find the number that is larger than a[0]
+			while(i <= hi && a[i] < a[lo]) i++; 
+			//Start from right find the number that is smaller than a[0]
+			while(j > lo && a[lo] < a[--j]);
+			//if i >= j, end search, else swap i and j
+			if(i >= j){
+				break;
+			}
+			swap(a, i, j);
+		}
+		swap(a, lo, j);
+		return j;
+	}
 	
 	
 	/**Self-built min-heap solution
 	 * O(nlogk) complexity and O(1) space
 	 * use given array to build a min-heap
 	 */
-	public int findKthLargest(int[] nums, int k){
+	public int findKthLargest3(int[] nums, int k){
 		if(k > nums.length) return -1;
 		//build a min-heap for previous k elements in nums
 		for(int i = 0; i < k; i++){
