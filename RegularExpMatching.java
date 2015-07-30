06d170624ae12db136dacb53e2391ffdc4c3c34c
zhiyanglu <lu_zhiyang@hotmail.com>
2015-07-30 14:10:18 -0700

    Add comments

--- RegularExpMatching.java
+++ RegularExpMatching.java
@@ -1,3 +1,13 @@
+/**
+ * Implement regular expression matching with support for '.' and '*'.
+ * 
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
+ * 
+ * The matching should cover the entire input string (not partial).
+ * The function prototype should be:
+ * bool isMatch(const char *s, const char *p)
+ */
 public class RegularExpMatching {
 
 	public static void main(String[] args) {
@@ -5,6 +15,17 @@ public class RegularExpMatching {
 
 	}
 
+	
+	/**
+	 * Matching Cases:
+	 * case 1: OPT[i-1][j-1] is true, and ith character of s is equal to jth character 
+	 * 		   of p. Or j th character of p is '.'
+	 * case 2: OPT[i-1][j] is true, then my pattern now is '*' and preceding character 
+	 * 		   is equal to incoming character of s
+	 * case 3: OPT[i][j-1] is true, then my pattern now is '*' which can match an empty 
+	 * 		   string
+	 * case 4: OPT[i][j-2] is true, and the pattern like (a*) matches an empty string
+	 */
 	public static boolean isMatch(String s, String p) {
 		if (s == null || p == null)
 			return false;
@@ -23,13 +44,19 @@ public class RegularExpMatching {
 
 		for (int i = 1; i <= m; i++) {
 			for (int j = 1; j <= n; j++) {
+				//add one char at string and pattern and two chars are same
 				if (Opt[i - 1][j - 1] && equals(s, p, i - 1, j - 1)) {
 					Opt[i][j] = true;
 				}
+				//add one char at string: current pattern is '*' and new added string
+				//is the same char before *
+				//add one char at pattern: new added pattern is '*'
 				if ((Opt[i - 1][j] || Opt[i][j - 1]) && p.charAt(j - 1) == '*'
 						&& equals(s, p, i - 1, j - 2)) {
 					Opt[i][j] = true;
 				}
+				//add two more char at pattern: add one char and a '*' to match
+				//empty string
 				if (j - 2 >= 0 && Opt[i][j - 2] && p.charAt(j - 1) == '*') {
 					Opt[i][j] = true;
 				}
