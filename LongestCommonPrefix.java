541cf03c3499cc8860dade25a358a2604f7ecdf4
zhiyanglu <lu_zhiyang@hotmail.com>
2015-07-31 10:57:03 -0700

    Longest Common Prefix

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,27 @@
+/**
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ *
+ */
+public class LongestCommonPrefix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0) return "";
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < strs[0].length(); i++){
+            for(String s : strs){
+                if(i >= s.length() || strs[0].charAt(i) != s.charAt(i))
+                    return sb.toString();
+            }
+            sb.append(strs[0].charAt(i));
+        }
+        return sb.toString();
+    }
+
+
+}
